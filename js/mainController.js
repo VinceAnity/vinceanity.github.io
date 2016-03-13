7e72c0ce2b7a60377c52f709161df909801d05d7
Vincent Longpré <vincent.longpre@gmail.com>
2016-03-13 14:13:09 -0400

    Switched API

--- js/mainController.js
+++ js/mainController.js
@@ -5,24 +5,21 @@ app.controller("mainController", function($scope, $http) {
             //Focus the editor on load
             editor.on("change", function() {
                 $http({
-                    url: 'https://api.aylien.com/api/v1/concepts',
+                    url: 'http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords',
                     method: 'GET',
-                    headers: {
-                        'X-AYLIEN-TextAPI-Application-ID': '8d91dbab',
-                        'X-AYLIEN-TextAPI-Application-Key': "18ebd80c679342549fd098293c0cebcf"
-                    },
                     params: {
-                        'text': $scope.tinymceModel
+                        'apikey': 'cd8e58f29967a60d898bdf9810f16a97e0206ad6',
+                        'text': $scope.tinymceModel,
+                        'outputMode': 'json'
                     }
                 }).then(function(response) {
-                    $scope.status = response.status;
-                    $scope.concepts = response.data;
-
-                    var keyword1 = response.data.concepts[Object.keys(response.data.concepts)[0]].surfaceForms[0].string;
-                    var keyword2 = response.data.concepts[Object.keys(response.data.concepts)[1]].surfaceForms[0].string;
+                    var keyword1 = response.data.keywords[0].text;
+                    var keyword2 = response.data.keywords[1].text;
 
                     console.log("keyword1 " + keyword1);
                     console.log("keyword2 " + keyword2);
+
+                    $scope.keywords = keyword1 + " " + keyword2;
 
                     $http({
                         url: 'https://api.shutterstock.com/v2/images/search',
@@ -31,10 +28,25 @@ app.controller("mainController", function($scope, $http) {
                             'Authorization': 'Basic ZGNiN2M3NTU0Y2Q4ZjcwMjk0NmE6MjM5ZDI4M2NiOGRiYmEzN2JjZDI3MmY4MDdmYjQzMjNmZDQ2Mzc0MQ=='
                         },
                         params: {
-                            'query': keyword1 + " " + keyword2
+                            'query': keyword1
                         }
                     }).then(function(response) {
-                        $scope.images = response.data.data;
+                        $scope.images1 = response.data.data;
+                    }, function(response) {
+                        console.log("Rip en pièces.");
+                    });
+
+                    $http({
+                        url: 'https://api.shutterstock.com/v2/images/search',
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Basic ZGNiN2M3NTU0Y2Q4ZjcwMjk0NmE6MjM5ZDI4M2NiOGRiYmEzN2JjZDI3MmY4MDdmYjQzMjNmZDQ2Mzc0MQ=='
+                        },
+                        params: {
+                            'query': keyword2
+                        }
+                    }).then(function(response) {
+                        $scope.images2 = response.data.data;
                     }, function(response) {
                         console.log("Rip en pièces.");
                     });
