ea9688b9bc1dfb2b0c114151d18d5d156ecc7635
Vincent Longpré <vincent.longpre@gmail.com>
2016-03-13 12:00:35 -0400

    Bower things

--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 # CS Games Long Running Marketing.ai challenge
 ## Installation
+    bower install
     npm install -g http-server
     http-server
 
--- js/mainController.js
+++ js/mainController.js
@@ -1,18 +1,42 @@
 app.controller("mainController", function($scope, $http) {
-    var ssImageSearchUrl = '';
+    $scope.tinymceOptions = {
+        setup: function(editor) {
+            console.log("text changed.");
 
-    $http.get({
-        url: 'https://api.shutterstock.com/v2/images/search',
-        method: 'GET',
-        headers: {
-            'Authorization': 'Basic ZGNiN2M3NTU0Y2Q4ZjcwMjk0NmE6MjM5ZDI4M2NiOGRiYmEzN2JjZDI3MmY4MDdmYjQzMjNmZDQ2Mzc0MQ=='
-        },
-        params: {
-            'query': 'drum'
+            //Focus the editor on load
+            editor.on("change", function() {
+                $http({
+                    url: 'https://api.aylien.com/api/v1/concepts',
+                    method: 'GET',
+                    headers: {
+                        'X-AYLIEN-TextAPI-Application-ID': '8d91dbab',
+                        'X-AYLIEN-TextAPI-Application-Key': "18ebd80c679342549fd098293c0cebcf"
+                    },
+                    params: {
+                        'text': 'drum'
+                    }
+                }).then(function(response) {
+                    var keyword1 = response.concepts[0].surfaceForms[0].string;
+                    var keyword2 = response.concepts[1].surfaceForms[0].string;
+
+                    $http({
+                        url: 'https://api.shutterstock.com/v2/images/search',
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Basic ZGNiN2M3NTU0Y2Q4ZjcwMjk0NmE6MjM5ZDI4M2NiOGRiYmEzN2JjZDI3MmY4MDdmYjQzMjNmZDQ2Mzc0MQ=='
+                        },
+                        params: {
+                            'query': '{keyword1} {keyword2}'
+                        }
+                    }).then(function(response) {
+                        $scope.images = response.data.data;
+                    }, function(response) {
+                        $scope.images = response.data.data;
+                    });
+                }, function(response) {
+                    console.log("Rip en pièces.")
+                });
+            });
         }
-    }).then(function(response) {
-        $scope.imageSearch = response.data;
-    }, function(response) {
-        $scope.imageSearch = response.data;
-    });
+    };
 });
